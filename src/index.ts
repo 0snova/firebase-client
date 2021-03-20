78fd09f3d433a9c1717a4aff3b79b6277f901c0f
Alexander Snov <snov.contact@gmail.com>
2021-03-21 02:27:04 +0700

    feat: add `onCollection` operator

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ export type { FirebaseAppOptions, FirebaseApp, firebase } from './firebase-app';
 export type { FirebaseEmulatorOptions } from './firebase-emulator';
 export { shouldUseEmulatorWhenLocalhost } from './firebase-emulator';
 
+export type { OnCollectionOptions, OnDocumentChange } from './operators/on-collection';
+export { onCollection } from './operators/on-collection';
+
 export default loadFirebase;
---
+++ src/operators/on-collection.ts
@@ -0,0 +1,48 @@
+import { FirebaseApp, firebase } from '../firebase-app';
+
+export type OnDocumentChange<T> = (
+  data: T,
+  id: string,
+  snapshot: firebase.firestore.DocumentSnapshot,
+  changeType: firebase.firestore.DocumentChangeType
+) => void;
+
+export type OnCollectionOptions<T> = {
+  removeAfterProcessed: boolean;
+  onNewDoc?: OnDocumentChange<T>;
+  onModifiedDoc?: OnDocumentChange<T>;
+  onRemovedDoc?: OnDocumentChange<T>;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function onCollection<T>(
+  firebase: FirebaseApp,
+  collection: string,
+  { onModifiedDoc, onNewDoc, onRemovedDoc, removeAfterProcessed: autoRemove }: OnCollectionOptions<T>
+) {
+  return firebase
+    .firestore()
+    .collection(collection)
+    .onSnapshot((snap) => {
+      snap.docChanges().forEach((change) => {
+        const docId = change.doc.id;
+        const data = change.doc.data();
+
+        switch (change.type) {
+          case 'added':
+            onNewDoc?.(data as T, docId, change.doc, change.type);
+            break;
+          case 'modified':
+            onModifiedDoc?.(data as T, docId, change.doc, change.type);
+            break;
+          case 'removed':
+            onRemovedDoc?.(data as T, docId, change.doc, change.type);
+            break;
+        }
+
+        if (change.type !== 'removed' && autoRemove) {
+          firebase.firestore().doc(`${collection}/${docId}`).delete();
+        }
+      });
+    });
+}
